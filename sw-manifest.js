255d463a38e4d89d70ec38002de25d84962b4330
dalelalmuslim <yosefselim885@gmail.com>
2026-05-03 18:05:33 +0300

    regenerate sw manifest for quran semantic anchors

--- sw-manifest.js
+++ sw-manifest.js
@@ -263,7 +263,7 @@ self.__SW_MANIFEST__ = Object.freeze({
   ],
   "precacheStats": {
     "documentsKb": 92.3,
-    "shellKb": 1831.6,
+    "shellKb": 1834.4,
     "dataKb": 1209.1,
     "deferredDataKb": 1724.8,
     "warmDataKb": 3.4
@@ -528,5 +528,5 @@ self.__SW_MANIFEST__ = Object.freeze({
   "routing": {
     "documentFallback": "./index.html"
   },
-  "cacheVersion": "azkar-v26-phase22-cd6411dd8783"
+  "cacheVersion": "azkar-v26-phase22-c395e2455b73"
 });
