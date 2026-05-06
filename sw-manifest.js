fcef1271f20c943eadd29ad21172f6e475d0ff8c
dalelalmuslim <yosefselim885@gmail.com>
2026-05-06 19:58:25 +0300

    regenerate sw manifest after duas reader polish

--- sw-manifest.js
+++ sw-manifest.js
@@ -257,7 +257,7 @@ self.__SW_MANIFEST__ = Object.freeze({
   ],
   "precacheStats": {
     "documentsKb": 90.2,
-    "shellKb": 1810.5,
+    "shellKb": 1810.8,
     "dataKb": 1209.1,
     "deferredDataKb": 1724.8,
     "warmDataKb": 3.4
@@ -516,5 +516,5 @@ self.__SW_MANIFEST__ = Object.freeze({
   "routing": {
     "documentFallback": "./index.html"
   },
-  "cacheVersion": "azkar-v26-phase22-f3ae4e81abe2"
+  "cacheVersion": "azkar-v26-phase22-3627366a6274"
 });
